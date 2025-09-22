f844ea2da48f80b22f515cfd02eb3374187bfaee
mansii1 <mansimangukiya0@gmail.com>
2025-09-22 22:49:28 +0530

    Completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
 import "./style.css";
+
+const textEl = document.getElementById("text") as HTMLElement;
+const speedEl = document.getElementById("speed") as HTMLInputElement;
+
+const text = "We Love Programming!";
+
+let index = 1;
+let speed = 300 / Number(speedEl?.value);
+
+writeText();
+
+function writeText() {
+  textEl.innerText = text.slice(0, index);
+  index++;
+
+  if (index > text.length) {
+    index = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener("input", (e) => {
+  const target = e.target as HTMLInputElement;
+  speed = 300 / Number(target);
+});
